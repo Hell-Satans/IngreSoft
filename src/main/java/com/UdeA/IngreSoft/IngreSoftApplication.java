502f0f022e795a2a0768107b7ff4e2c6e1755c4f
mike <sebasra@yahoo.com>
2022-08-28 14:22:14 -0500

    Se crea proyecto IngreSoft

---
+++ src/main/java/com/UdeA/IngreSoft/IngreSoftApplication.java
@@ -0,0 +1,13 @@
+package com.UdeA.IngreSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngreSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngreSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UdeA/IngreSoft/IngreSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UdeA.IngreSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngreSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
