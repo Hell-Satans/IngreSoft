ef50a38a6bccd04e01668f944bcfcd84cb103727
Henry <111475591+Henryar7@users.noreply.github.com>
2022-09-09 15:59:45 -0500

    Crear tabla Perfil

--- src/main/java/com/UdeA/IngreSoft/Entidad/Perfil.java
+++ src/main/java/com/UdeA/IngreSoft/Entidad/Perfil.java
@@ -1,16 +1,16 @@
 package com.UdeA.IngreSoft.Entidad;
 
 public class Perfil {
-    private String idPerfil;
-    private String imagenPerfil;
-    private Empleado telefono;
-    private String usuarioPerfil;
-    private Empleado cargo;
+    private java.lang.String idPerfil;
+    private java.lang.String imagenPerfil;
+    private String telefono;
+    private java.lang.String usuarioPerfil;
+    private String cargo;
 
     public Perfil() {
     }
 
-    public Perfil(String idPerfil, String imagenPerfil, Empleado telefono, String usuarioPerfil, Empleado cargo) {
+    public Perfil(java.lang.String idPerfil, java.lang.String imagenPerfil, String telefono, java.lang.String usuarioPerfil, String cargo) {
         this.idPerfil = idPerfil;
         this.imagenPerfil = imagenPerfil;
         this.telefono = telefono;
@@ -18,48 +18,48 @@ public class Perfil {
         this.cargo = cargo;
     }
 
-    public String getIdPerfil() {
+    public java.lang.String getIdPerfil() {
         return idPerfil;
     }
 
-    public void setIdPerfil(String idPerfil) {
+    public void setIdPerfil(java.lang.String idPerfil) {
         this.idPerfil = idPerfil;
     }
 
-    public String getImagenPerfil() {
+    public java.lang.String getImagenPerfil() {
         return imagenPerfil;
     }
 
-    public void setImagenPerfil(String imagenPerfil) {
+    public void setImagenPerfil(java.lang.String imagenPerfil) {
         this.imagenPerfil = imagenPerfil;
     }
 
-    public Empleado getTelefono() {
+    public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(Empleado telefono) {
+    public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    public String getUsuarioPerfil() {
+    public java.lang.String getUsuarioPerfil() {
         return usuarioPerfil;
     }
 
-    public void setUsuarioPerfil(String usuarioPerfil) {
+    public void setUsuarioPerfil(java.lang.String usuarioPerfil) {
         this.usuarioPerfil = usuarioPerfil;
     }
 
-    public Empleado getCargo() {
+    public String getCargo() {
         return cargo;
     }
 
-    public void setCargo(Empleado cargo) {
+    public void setCargo(String cargo) {
         this.cargo = cargo;
     }
 
     @Override
-    public String toString() {
+    public java.lang.String toString() {
         return "Perfil{" +
                 "idPerfil='" + idPerfil + '\'' +
                 ", imagenPerfil='" + imagenPerfil + '\'' +
